32a0f24dcb73f3ba26c35c2a429794679b7fa0f4
jessie wang <117032848+jwaanng@users.noreply.github.com>
2023-11-02 17:18:06 -0400

    mobile nav trial fix

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -27,7 +27,7 @@ const AboutMe = () => {
       greeting: "Welcome! I'm Jessie, a second year undergrad student at UofT interested in and studying cognitive science, computer science, and math (sometimes). I enjoy trying and cooking new foods, playing badminton, creating art, making ideas come to life with code, and sharing my life. Feel free to check out some of my projects or reach out to me!",
     },
     zh: {
-      greeting: "欢迎！我是Jessie,一名在多伦多大学的二年级本科学生，我对认知科学，计算机科学，和数学感兴趣。我享受尝试并制作新的食物，打羽毛球，创作艺术，用代码创造新事物， 和分享我的生活。有空可以来研究研究我的作品😊",
+      greeting: "欢迎！我是Jessie,一名在多伦多大学的二年级本科学生，我对认知科学，计算机科学，和数学感兴趣。我享受尝试并制作新的食物，打羽毛球，创作艺术，用代码创造新事物， 和分享我的生活。有空可以来研究研究我的作品",
     },
   };
 
