c0fa82baaa2e69f13a87ded95418dbfe59749ba8
jessie wang <117032848+jwaanng@users.noreply.github.com>
2023-11-02 17:31:12 -0400

    changed abt text

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -24,10 +24,10 @@ const AboutMe = () => {
 
   const text = {
     en: {
-      greeting: "Welcome! I'm Jessie, a second year undergrad student at UofT interested in and studying cognitive science, computer science, and math (sometimes). I enjoy trying and cooking new foods, playing badminton, creating art, making ideas come to life with code, and sharing my life. Feel free to check out some of my projects or reach out to me!",
+      greeting: "Welcome! I'm Jessie, a second year undergrad student at UofT interested in and studying cognitive science, computer science, and math (sometimes). I enjoy trying and cooking new foods, playing badminton, creating art, making ideas come to life with code, and sharing my life. Feel free to check out some of my projects and reach out to me!",
     },
     zh: {
-      greeting: "欢迎！我是Jessie,一名在多伦多大学的二年级本科学生，我对认知科学，计算机科学，和数学感兴趣。我享受尝试并制作新的食物，打羽毛球，创作艺术，用代码创造新事物， 和分享我的生活。有空可以来研究研究我的作品",
+      greeting: "欢迎！我是Jessie，目前是多伦多大学本科二年级的学生。我擅长认知科学，计算机科学和数学等相关领域的学习，同时对美食制作，艺术创作，编程和打羽毛球非常感兴趣。我很乐意向你分享我的生活，欢迎关注我的作品！",
     },
   };
 
